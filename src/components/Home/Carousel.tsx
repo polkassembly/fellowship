eb92bfd51103e1175628d0e1b400ca99aa0df38a
alphainfinitus <kumar.mridul000@gmail.com>
2023-10-23 11:56:23 +0530

    feat: :sparkles: Carousel placeholder

--- src/components/Home/Carousel.tsx
+++ src/components/Home/Carousel.tsx
@@ -2,10 +2,27 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import Image from 'next/image';
 import React from 'react';
 
+const images = ['/carousel/carousel-1.svg'];
+
 function Carousel() {
-	return <div>Carousel</div>;
+	return (
+		<div className='flex'>
+			{images.map((image, index) => (
+				<div key={image}>
+					<Image
+						src={image}
+						alt={`Slide ${index}`}
+						className='h-full w-[100vw]'
+						width={500}
+						height={300}
+					/>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default Carousel;
