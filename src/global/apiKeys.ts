db4b1934844d2f2f199f30502eb8667f36a5d429
alphainfinitus <kumar.mridul000@gmail.com>
2023-11-02 20:19:19 +0530

    feat: :wrench: apiKeys.ts file

---
+++ src/global/apiKeys.ts
@@ -0,0 +1,18 @@
+// Copyright 2019-2025 @polkassembly/fellowship authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+// TODO: remove unused keys
+
+export const FIREBASE_SERVICE_ACC_CONFIG = process.env.FIREBASE_SERVICE_ACC_CONFIG || '';
+export const JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY || '';
+export const JWT_PUBLIC_KEY = process.env.JWT_PUBLIC_KEY || '';
+export const JWT_KEY_PASSPHRASE = process.env.JWT_KEY_PASSPHRASE || '';
+export const REDIS_URL = process.env.REDIS_URL || '';
+export const REPORTS_THRESHOLD = Number(process.env.REPORTS_THRESHOLD) || 10;
+export const GIPHY_API_KEY = process.env.NEXT_PUBLIC_GIPHY_API_KEY || '';
+export const TINY_MCE_API_KEY = process.env.NEXT_PUBLIC_TINY_MCE_API_KEY || '';
+export const ALGOLIA_APP_ID = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || '';
+export const ALGOLIA_SEARCH_API_KEY = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || '';
+export const SUBSCAN_API_KEY = process.env.NEXT_PUBLIC_SUBSCAN_API_KEY || '';
+export const IMG_BB_API_KEY = process.env.NEXT_PUBLIC_IMG_BB_API_KEY || '';
