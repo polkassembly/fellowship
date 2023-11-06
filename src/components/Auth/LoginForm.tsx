ef768f465b5aadfac9df9b18452395cbe2ea69c3
alphainfinitus <kumar.mridul000@gmail.com>
2023-11-06 22:07:41 +0530

    feat: :sparkles: TFA form

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -9,7 +9,7 @@ import { Input } from '@nextui-org/input';
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { PASSWORD_RULES, USERNAME_RULES } from '@/global/validationRules';
+import { PASSWORD_RULES, TFA_CODE_RULES, USERNAME_RULES } from '@/global/validationRules';
 import { IAuthResponse } from '@/global/types';
 import nextApiClientFetch from '@/utils/nextApiClientFetch';
 import { handleTokenChange } from '@/services/auth.service';
@@ -18,16 +18,28 @@ import { useRouter } from 'next/navigation';
 import WalletButtonsRow from './WalletButtonsRow';
 import AlertCard from '../Misc/AlertCard';
 
+const INPUT_LABEL_CLASSNAMES = 'text-sm font-normal';
+const INPUT_WRAPPER_CLASSNAMES = 'border-primary_border border-1';
+
 function LoginForm() {
 	const {
 		register,
 		formState: { errors },
-		// control,
 		handleSubmit
 	} = useForm({
 		defaultValues: {
 			usernameOrEmail: '',
 			password: ''
+		}
+	});
+
+	const {
+		register: registerTFAForm,
+		formState: { errors: errorsTFAForm },
+		handleSubmit: handleSubmitTFAForm
+	} = useForm({
+		defaultValues: {
+			authCode: ''
 		}
 	});
 
@@ -42,6 +54,7 @@ function LoginForm() {
 		if (!usernameOrEmail || !password || loading) return;
 
 		setLoading(true);
+		setError('');
 
 		const { data, error: loginError } = await nextApiClientFetch<IAuthResponse>({
 			url: 'api/v1/auth/actions/login',
@@ -72,92 +85,175 @@ function LoginForm() {
 		}
 	};
 
-	// TODO: Add TFA form
+	const onTFASubmit = async ({ authCode }: { authCode: string }) => {
+		if (Number.isNaN(authCode)) return;
+
+		setLoading(true);
+		setError('');
+
+		const { data, error: tfaError } = await nextApiClientFetch<IAuthResponse>({
+			url: 'api/v1/auth/actions/2fa/validate',
+			isPolkassemblyAPI: true,
+			data: {
+				auth_code: String(authCode), // use string for if it starts with 0
+				tfa_token: authResponse?.tfa_token,
+				user_id: Number(authResponse?.user_id)
+			}
+		});
+
+		if (tfaError || !data) {
+			setError(tfaError || 'Login failed. Please try again later.');
+			setLoading(false);
+			return;
+		}
+
+		if (data?.token) {
+			setError('');
+			handleTokenChange(data.token, currentUser);
+			router.back();
+		}
+	};
 
 	return (
 		<div className='flex flex-col gap-6 p-3 text-sm'>
 			{error && <AlertCard message={error} />}
 
 			{authResponse?.isTFAEnabled ? (
-				<>TFA is enabled for this account. Please enter the code from your authenticator app.</>
-			) : (
-				<form
-					onSubmit={handleSubmit(onSubmit)}
-					className='flex flex-col gap-3'
-				>
-					<div>
-						<Input
-							label='Username or Email'
-							placeholder='Type here'
-							labelPlacement='outside'
-							variant='bordered'
-							radius='sm'
-							classNames={{
-								label: 'text-sm font-normal',
-								inputWrapper: 'border-primary_border border-1'
-							}}
-							isInvalid={Boolean(errors.usernameOrEmail)}
-							disabled={loading}
-							{...register('usernameOrEmail', { required: true, minLength: USERNAME_RULES.minLength })}
-						/>
-						{Boolean(errors.usernameOrEmail) && <small className='text-danger'>Please input a vaild username or email.</small>}
-					</div>
-
-					<div>
-						<Input
-							label='Password'
-							type='password'
-							placeholder='Type here'
-							labelPlacement='outside'
-							variant='bordered'
-							radius='sm'
-							classNames={{
-								label: 'text-sm font-normal',
-								inputWrapper: 'border-primary_border border-1'
-							}}
-							isInvalid={Boolean(errors.password)}
-							disabled={loading}
-							{...register('password', { required: true, minLength: PASSWORD_RULES.minLength })}
-						/>
-						{Boolean(errors.password) && <small className='text-danger'>Please input a vaild password.</small>}
-
-						<div className='mt-1.5 flex w-full justify-end'>
-							<Link
-								href='/forgot-password'
-								className='text-xs text-primary'
-							>
-								Forgot Password?
-							</Link>
+				<>
+					<form
+						onSubmit={handleSubmitTFAForm(onTFASubmit)}
+						className='flex flex-col gap-3'
+					>
+						<div className='flex flex-col gap-3'>
+							<h3 className='text-lg font-medium'>Two Factor Authentication</h3>
+							<p>Please open the two-step verification app or extension and input the authentication code for your Polkassembly account.</p>
 						</div>
-					</div>
 
-					<div className='flex justify-center'>
-						<Button
-							color='primary'
-							className='w-3/6'
-							type='submit'
-							isLoading={loading}
+						<div>
+							<Input
+								label='Authentication Code'
+								placeholder='######'
+								labelPlacement='outside'
+								variant='bordered'
+								radius='sm'
+								classNames={{
+									label: INPUT_LABEL_CLASSNAMES,
+									inputWrapper: INPUT_WRAPPER_CLASSNAMES
+								}}
+								isInvalid={Boolean(errorsTFAForm.authCode)}
+								disabled={loading}
+								{...registerTFAForm('authCode', { required: true, minLength: TFA_CODE_RULES.minLength, maxLength: TFA_CODE_RULES.maxLength })}
+							/>
+							{Boolean(errorsTFAForm.authCode) && <small className='text-danger'>Please input a vaild authentication code.</small>}
+						</div>
+
+						<div className='flex justify-center'>
+							<Button
+								color='primary'
+								className='w-3/6'
+								type='submit'
+								isLoading={loading}
+							>
+								Login
+							</Button>
+						</div>
+					</form>
+
+					<Button
+						color='default'
+						variant='faded'
+						className='mx-auto border-none'
+						type='button'
+						disabled={loading}
+						size='sm'
+						onClick={() => {
+							setAuthResponse(null);
+							setError('');
+						}}
+					>
+						Go back
+					</Button>
+				</>
+			) : (
+				<>
+					<form
+						onSubmit={handleSubmit(onSubmit)}
+						className='flex flex-col gap-3'
+					>
+						<div>
+							<Input
+								label='Username or Email'
+								placeholder='Type here'
+								labelPlacement='outside'
+								variant='bordered'
+								radius='sm'
+								classNames={{
+									label: INPUT_LABEL_CLASSNAMES,
+									inputWrapper: INPUT_WRAPPER_CLASSNAMES
+								}}
+								isInvalid={Boolean(errors.usernameOrEmail)}
+								disabled={loading}
+								{...register('usernameOrEmail', { required: true, minLength: USERNAME_RULES.minLength })}
+							/>
+							{Boolean(errors.usernameOrEmail) && <small className='text-danger'>Please input a vaild username or email.</small>}
+						</div>
+
+						<div>
+							<Input
+								label='Password'
+								type='password'
+								placeholder='Type here'
+								labelPlacement='outside'
+								variant='bordered'
+								radius='sm'
+								classNames={{
+									label: INPUT_LABEL_CLASSNAMES,
+									inputWrapper: INPUT_WRAPPER_CLASSNAMES
+								}}
+								isInvalid={Boolean(errors.password)}
+								disabled={loading}
+								{...register('password', { required: true, minLength: PASSWORD_RULES.minLength })}
+							/>
+							{Boolean(errors.password) && <small className='text-danger'>Please input a vaild password.</small>}
+
+							<div className='mt-1.5 flex w-full justify-end'>
+								<Link
+									href='/forgot-password'
+									className='text-xs text-primary'
+								>
+									Forgot Password?
+								</Link>
+							</div>
+						</div>
+
+						<div className='flex justify-center'>
+							<Button
+								color='primary'
+								className='w-3/6'
+								type='submit'
+								isLoading={loading}
+							>
+								Login
+							</Button>
+						</div>
+					</form>
+
+					<div className='mt-4 text-center text-gray-500'>Or login with</div>
+
+					<WalletButtonsRow disabled={loading} />
+
+					<div className='w-full text-center font-semibold'>
+						Don&apos;t have an account?{' '}
+						<Link
+							href='/signup'
+							className='text-sm text-primary'
+							replace
 						>
-							Login
-						</Button>
+							Sign Up
+						</Link>
 					</div>
-				</form>
+				</>
 			)}
-
-			<div className='mt-4 text-center text-gray-500'>Or login with</div>
-
-			<WalletButtonsRow disabled={loading} />
-
-			<div className='w-full text-center font-semibold'>
-				Don&apos;t have an account?{' '}
-				<Link
-					href='/signup'
-					className='text-sm text-primary'
-					replace
-				>
-					Sign Up
-				</Link>
-			</div>
 		</div>
 	);
 }
--- src/global/validationRules.ts
+++ src/global/validationRules.ts
@@ -6,3 +6,5 @@ export const USERNAME_RULES = { maxLength: 30, minLength: 3, pattern: /^[A-Za-z0
 export const EMAIL_RULES = { pattern: /^[A-Z0-9_'%=+!`#~$*?^{}&|-]+([.][A-Z0-9_'%=+!`#~$*?^{}&|-]+)*@[A-Z0-9-]+(\.[A-Z0-9-]+)+$/i };
 
 export const PASSWORD_RULES = { minLength: 6 };
+
+export const TFA_CODE_RULES = { minLength: 6, maxLength: 6, length: 6 };
