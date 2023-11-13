13e9abf19b5c7168a7f1a72d095f44f7ea66bcb8
alphainfinitus <kumar.mridul000@gmail.com>
2023-11-13 07:06:18 +0530

    style: :recycle: root loading.tsx uses LoadingSpinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,13 +2,13 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { Spinner } from '@nextui-org/spinner';
+import LoadingSpinner from '@/components/Misc/LoadingSpinner';
 import React from 'react';
 
 function Loading() {
 	return (
 		<section className='flex min-h-[25vh] flex-col items-center justify-center'>
-			<Spinner />
+			<LoadingSpinner />
 		</section>
 	);
 }
