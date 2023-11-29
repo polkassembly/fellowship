fb162c57b5e0fbffd1b9124bac26032de47e4174
alphainfinitus <kumar.mridul000@gmail.com>
2023-11-29 21:35:20 +0530

    feat: :sparkles: coming soon pages

---
+++ src/app/(site)/profile/page.tsx
@@ -0,0 +1,19 @@
+// Copyright 2019-2025 @polkassembly/fellowship authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import ComingSoon from '@/components/Misc/ComingSoon';
+import React from 'react';
+
+function ProfilePage() {
+	return (
+		<div className='flex h-[50vh] w-full items-center justify-center'>
+			<ComingSoon
+				imgSize={256}
+				className='text-lg'
+			/>
+		</div>
+	);
+}
+
+export default ProfilePage;
