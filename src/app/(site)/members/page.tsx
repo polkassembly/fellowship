ba5e0eb2320d93c4b425efe29c2882daa7b0fc85
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2024-07-10 06:20:10 +0100

    fix scrollbar on members table  mobile

--- src/app/(site)/members/page.tsx
+++ src/app/(site)/members/page.tsx
@@ -72,7 +72,7 @@ function MembersPage() {
 
 			<TopFellowsCard fellowsDetails={fellowsDetails} />
 
-			<div className='mt-9 flex w-full gap-3 overflow-x-scroll'>
+			<div className='mt-9 flex w-full gap-3 overflow-x-scroll scrollbar-hide'>
 				<Chip
 					onClick={() => setSelectedRankGroup(null)}
 					color={!selectedRankGroup ? 'primary' : 'default'}
