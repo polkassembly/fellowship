2929fd3f16b366f8b67994818f38011a711cd108
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2024-09-23 21:59:27 +0100

    navigate to pending

--- src/components/Home/PendingTasks.tsx
+++ src/components/Home/PendingTasks.tsx
@@ -6,7 +6,9 @@
 
 import { Card } from '@nextui-org/card';
 import React, { useEffect, useState, cache } from 'react';
+import { useRouter } from 'next/navigation';
 import { useApiContext } from '@/contexts';
+import { EActivityFeed } from '@/global/types';
 import LoadingSpinner from '../Misc/LoadingSpinner';
 import Progress from '../Misc/Progress';
 
@@ -15,22 +17,22 @@ const getPendingTasks = cache(() => {
 });
 
 function PendingTasks({ className }: Readonly<{ className?: string }>) {
-	const { api, apiReady } = useApiContext();
+	const { api, apiReady, network } = useApiContext();
+	const router = useRouter();
+
 	const [pendingTasks, setPendingTasks] = useState<number>(0);
 	const [allTasks, setAllTasks] = useState<number>(54);
 
 	const [loading, setLoading] = useState(true);
-
-	const [mounted, setMounted] = useState(false);
 
 	useEffect(() => {
 		async function fetchPendingTasks() {
 			if (!api || !apiReady) return;
 
 			try {
-				const pendingTasks = await getPendingTasks();
+				const pendingTaskCount = await getPendingTasks();
 
-				setPendingTasks(pendingTasks);
+				setPendingTasks(pendingTaskCount);
 				setLoading(false);
 			} catch (error) {
 				console.error('Error fetching pendingTasks:', error);
@@ -41,14 +43,9 @@ function PendingTasks({ className }: Readonly<{ className?: string }>) {
 		fetchPendingTasks();
 	}, [api, apiReady]);
 
-	// only mount on client to prevent hydration error
-	useEffect(() => {
-		setMounted(true);
-	}, []);
-
-	if (!mounted) {
-		return null;
-	}
+	const handleShowPendingTasks = (activityValue: string) => {
+		router.push(`/?feed=${activityValue}&network=${network}`);
+	};
 
 	return (
 		<Card
@@ -59,7 +56,13 @@ function PendingTasks({ className }: Readonly<{ className?: string }>) {
 				<LoadingSpinner message='fetching pending tasks...' />
 			) : (
 				<div>
-					<h1 className='mb-3'>Pending tasks &gt;</h1>
+					<button
+						type='button'
+						className='mb-3 cursor-pointer font-medium'
+						onClick={() => handleShowPendingTasks(EActivityFeed.PENDING)}
+					>
+						Pending tasks &gt;
+					</button>
 					<div className='flex w-full flex-row items-center gap-3'>
 						<div className='min-w-max rounded-full'>
 							<Progress
