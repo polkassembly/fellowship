70955b4ef37c7545a9a15c08ee4ccf37f66417fb
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2024-10-08 02:30:30 +0100

    fetch recording by Id

---
+++ src/app/api/v1/recordings/[id]/route.ts
@@ -0,0 +1,32 @@
+// Copyright 2019-2025 @polkassembly/fellowship authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import withErrorHandling from '@/app/api/api-utils/withErrorHandling';
+import { NextRequest, NextResponse } from 'next/server';
+import { APIError } from '@/global/exceptions';
+import { IRecording } from '@/global/types';
+import MESSAGES from '@/global/messages';
+import { API_ERROR_CODE } from '@/global/constants/errorCodes';
+import getReqBody from '@/app/api/api-utils/getReqBody';
+import { recordingDocRef } from '../../firestoreRefs';
+
+export const POST = withErrorHandling(async (req: NextRequest) => {
+	const { recordingId } = await getReqBody(req);
+
+	if (isNaN(recordingId)) throw new APIError(`${MESSAGES.REQ_BODY_ERROR}`, 500, API_ERROR_CODE.REQ_BODY_ERROR);
+
+	const querySnapshot = await recordingDocRef(recordingId).get();
+
+	const data = querySnapshot.data();
+
+	const recording = {
+		id: data?.id,
+		who: data?.proposer_address,
+		created_at: data?.created_at?.toDate?.() || new Date(),
+		updated_at: data?.updated_at?.toDate?.() || new Date(),
+		title: data?.title
+	} as IRecording;
+
+	return NextResponse.json(recording as IRecording);
+});
